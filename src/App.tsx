2ecf60aa16343134ccdd2c278cf169b2129a15aa
kansg92 <kansg922@gmail.com>
2023-05-01 17:20:23 +0900

    back-end 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/v2/Recap/test")
+      .then((response) => response.text())
+      .then((message) => {
+        setMessage(message);
+      });
+  }, []);
+  console.log(message);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Hi</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -16,7 +27,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React Recap Start!!
         </a>
       </header>
     </div>
